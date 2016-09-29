13a92351e29da366b1c5aa6807600778d928b307
Joe Kwong <scq198@hotmail.com>
2016-09-29 14:09:14 -0500

    Small UI tweaks

--- MovieLib/MovieLib/Form1.Designer.cs
+++ MovieLib/MovieLib/Form1.Designer.cs
@@ -307,7 +307,7 @@
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "My Movie Library";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.Movies_Data)).EndInit();
